5c6a2c5258800b5ff1e76a876dc226b3ae3ac2bd
EricMarinMejia <ericmarin14@hotmail.com>
2023-12-01 09:48:25 -0500

    Création de la vue PDF

---
+++ resources/views/my-pdf-file.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pro-Auto Experts</title>
+    </head>
+    <body>
+        <div style="text-align: center;">
+            <h1>Bienvenue sur le site de Pro-Auto Experts</h1>
+        </div>
+        <p><strong>Titre: </strong>{{ $title }}</p>
+        <p><strong>Auteur: </strong>{{ $author }}</p>
+        <p><strong>Date: </strong>[09:46 AM] 2023-12-01</p>
+        <br />
+        <p>Malheuresement, ce PDF ne contient aucune information utile
+            pour le moment. Et pour être honnête, il ne contiendra probablement
+            jamais aucune autre information. Cependant, pour vous remonter le moral,
+            ce PDF contient une petite blague.
+        </p>
+        <br />
+        <p>
+            <strong>Quelle est la meilleure chose à propos de la Suisse?</strong><br />
+            <p>Je ne sais pas, mais le drapeau est un gros plus.</p>
+        </p>
+        <br />
+        <p>
+            Si cette blague ne vous apporte aucune joie, je suis navré.
+            Bonne journée!
+        <p>
+    </body>
+</html>

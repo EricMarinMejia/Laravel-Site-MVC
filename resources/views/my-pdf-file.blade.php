8adaf99ad28a4e60643714149e07a9cd474c080f
EricMarinMejia <ericmarin14@hotmail.com>
2023-12-01 10:40:49 -0500

    Ajustement du PDF

--- resources/views/my-pdf-file.blade.php
+++ resources/views/my-pdf-file.blade.php
@@ -14,17 +14,19 @@
         <p>Malheuresement, ce PDF ne contient aucune information utile
             pour le moment. Et pour être honnête, il ne contiendra probablement
             jamais aucune autre information. Cependant, pour vous remonter le moral,
-            ce PDF contient une petite blague.
+            ce PDF contient un "fun-fact" sur les crevettes pistolets.
         </p>
         <br />
         <p>
-            <strong>Un footballeur va chez le coiffeur</strong><br />
-            <p><strong>Coiffeur: </strong>Comment je coupe tes cheuveux aujourd'hui?</p>
-            <p><strong>Footballeur: </strong>En fait j'aimerais la coupe du monde</p>
+            Les crevettes pistolets possèdent une pince qu'ils utilisent pour se défendre
+            des prédateurs. Lorsqu'il ferment leur pince, ils tirent une bulle qui atteint 
+            une température de 7,700°C et produit un son de 210 décibels.
+            Pour référence, la température de la surface du soleil est de 5,500°C et 
+            un pistolet régulier produit un son d'environ 140 à 175 décibels.
         </p>
         <br />
         <p>
-            Si cette blague ne vous apporte aucune joie, je suis navré.
+            Si cette information ne vous apporte aucune joie, je suis navré.
             Bonne journée!
         <p>
     </body>

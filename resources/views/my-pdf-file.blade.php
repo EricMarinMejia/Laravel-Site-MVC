579442019dd5fab660a6fd7598a6e1b4ea9de161
EricMarinMejia <ericmarin14@hotmail.com>
2023-12-01 11:13:02 -0500

    Correction ortographe

--- resources/views/my-pdf-file.blade.php
+++ resources/views/my-pdf-file.blade.php
@@ -11,7 +11,7 @@
         <p><strong>Auteur: </strong>{{ $author }}</p>
         <p><strong>Date: </strong>[09:46 AM] 2023-12-01</p>
         <br />
-        <p>Malheuresement, ce PDF ne contient aucune information utile
+        <p>Malheureseument, ce PDF ne contient aucune information utile
             pour le moment. Et pour être honnête, il ne contiendra probablement
             jamais aucune autre information. Cependant, pour vous remonter le moral,
             ce PDF contient un "fun-fact" sur les crevettes pistolets.
@@ -19,7 +19,7 @@
         <br />
         <p>
             Les crevettes pistolets possèdent une pince qu'ils utilisent pour se défendre
-            des prédateurs. Lorsqu'il ferment leur pince, ils tirent une bulle qui atteint 
+            des prédateurs. Lorsqu'ils ferment leur pince, ils tirent une bulle qui atteint 
             une température de 7,700°C et produit un son de 210 décibels.
             Pour référence, la température de la surface du soleil est de 5,500°C et 
             un pistolet régulier produit un son d'environ 140 à 175 décibels.

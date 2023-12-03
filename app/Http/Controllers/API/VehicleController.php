3ce4b0d7539d349184ee0cddb9651875f620cc80
EricMarinMejia <ericmarin14@hotmail.com>
2023-12-03 13:57:32 -0500

    Controller methods Vehicle

--- app/Http/Controllers/API/VehicleController.php
+++ app/Http/Controllers/API/VehicleController.php
@@ -15,7 +15,8 @@ class VehicleController extends Controller
      */
     public function index()
     {
-        //
+        $vehicles = Vehicle::all();
+        return response()->json($vehicles);
     }
 
     /**
@@ -26,7 +27,54 @@ class VehicleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $today = now();
+
+        $request->validate([
+            'user_id' => 'required',
+            'brand' => 'required',
+            'model' => 'required',
+            'license_plate' => 'required',
+            'kilometers' => 'required',
+            'image' => 'image|mimes:jpg,png,jpeg,gif,svg|max:2048'
+        ]);
+
+
+        if ($request->file('image') != null) {
+
+            if ($request->file('image')->isValid()) {
+                $image = $request->file('image');
+                $filename = time() . '.' . $image->getClientOriginalExtension();
+                $path = $image->storeAs('images/upload', $filename, 'public');
+            }
+
+            $vehicle = new vehicle([
+                'created_at' => $today,
+                'updated_at' => $today,
+                'user_id' => $request->user_id,
+                'brand' => $request->brand,
+                'model' => $request->model,
+                'license_plate' => $request->license_plate,
+                'kilometers' => $request->kilometers,
+                'image' => $filename
+            ]);
+
+            return response()->json($vehicle, 201);
+        } else {
+
+            $vehicle = new vehicle([
+                'created_at' => $today,
+                'updated_at' => $today,
+                'user_id' => $request->user_id,
+                'brand' => $request->brand,
+                'model' => $request->model,
+                'license_plate' => $request->license_plate,
+                'kilometers' => $request->kilometers,
+                'image' => null
+            ]);
+
+            return response()->json($vehicle, 201);
+        }
+
     }
 
     /**
@@ -37,7 +85,7 @@ class VehicleController extends Controller
      */
     public function show(Vehicle $vehicle)
     {
-        //
+        return response()->json($vehicle);
     }
 
     /**
@@ -49,7 +97,53 @@ class VehicleController extends Controller
      */
     public function update(Request $request, Vehicle $vehicle)
     {
-        //
+        $today = now();
+
+        $request->validate([
+            'user_id' => 'required',
+            'brand' => 'required',
+            'model' => 'required',
+            'license_plate' => 'required',
+            'kilometers' => 'required',
+            'image' => 'image|mimes:jpg,png,jpeg,gif,svg|max:2048'
+        ]);
+
+
+        if ($request->file('image') != null) {
+
+            if ($request->file('image')->isValid()) {
+                $image = $request->file('image');
+                $filename = time() . '.' . $image->getClientOriginalExtension();
+                $path = $image->storeAs('images/upload', $filename, 'public');
+            }
+
+            $vehicle->update([
+                'created_at' => $today,
+                'updated_at' => $today,
+                'user_id' => $request->user_id,
+                'brand' => $request->brand,
+                'model' => $request->model,
+                'license_plate' => $request->license_plate,
+                'kilometers' => $request->kilometers,
+                'image' => $filename
+            ]);
+
+            return response()->json();
+        } else {
+
+            $vehicle->update([
+                'created_at' => $today,
+                'updated_at' => $today,
+                'user_id' => $request->user_id,
+                'brand' => $request->brand,
+                'model' => $request->model,
+                'license_plate' => $request->license_plate,
+                'kilometers' => $request->kilometers,
+                'image' => null
+            ]);
+
+            return response()->json();
+        }
     }
 
     /**
@@ -60,6 +154,8 @@ class VehicleController extends Controller
      */
     public function destroy(Vehicle $vehicle)
     {
-        //
+        $vehicle->delete();
+
+        return response()->json();
     }
 }
